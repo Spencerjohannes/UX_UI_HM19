6a5aa4e69407793283abb34358f52fca0f5af91a
Spencerjohannes <srjceramics@hotmail.com>
2024-02-24 11:09:30 -0600

    update

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.log("your index.js file is loaded correctly!");
+
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 800); 
+        }
+    });
+});
+
+
+
+
